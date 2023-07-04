6e1111be578946061cf7ab9cf5be2bab2ec6b5bc
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-07-04 23:14:11 +0300

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require_relative './user.rb'
+ 
+# Student < User
+#  def initialize
+#         @knowledge = []
+#  end
+#     def learn(any_string)
+#         @knowledge << any_string
+#     end
+# end
 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn (any_string)
+        @knowledge << any_string
+    end
+    def knowledge
+        @knowledge
+    end
 end
